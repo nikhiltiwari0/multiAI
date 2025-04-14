4701a23c2e5a5a816dcc4e30a4c340917753247c
Nikhil Tiwari <tiwarin9211@gmail.com>
2025-04-14 00:42:34 -0500

    updated vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Index from "./pages/Index";
-import Landing from "./pages/Landing";
-import Subscribe from "./pages/Subscribe";
-import NotFound from "./pages/NotFound";
+
 import Auth from "./pages/Auth";
 import { AuthProvider } from "@/contexts/AuthContext";
 import { ChatProvider } from "@/contexts/ChatContext";
+import Index from "./pages/Index";
+import Landing from "./pages/Landing";
+import NotFound from "./pages/NotFound";
 import { ProtectedRoute } from "./components/ProtectedRoute";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import Subscribe from "./pages/Subscribe";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
 
 const queryClient = new QueryClient();
 
@@ -26,11 +26,14 @@ const App = () => (
             <Routes>
               <Route path="/" element={<Landing />} />
               <Route path="/auth" element={<Auth />} />
-              <Route path="/app" element={
-                <ProtectedRoute>
-                  <Index />
-                </ProtectedRoute>
-              } />
+              <Route
+                path="/app"
+                element={
+                  <ProtectedRoute>
+                    <Index />
+                  </ProtectedRoute>
+                }
+              />
               <Route path="/subscribe" element={<Subscribe />} />
               {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
               <Route path="*" element={<NotFound />} />
